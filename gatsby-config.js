6c8c9f144137840cc77e01f46adb4d6e3a0ca472
Tyler Barnes <tylerdbarnes@gmail.com>
2020-11-19 12:11:37 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://gatsbypreview.wpengine.com/graphql`,
+          `https://wpgatsbydemo.wpengine.com/graphql`,
       },
     },
 
