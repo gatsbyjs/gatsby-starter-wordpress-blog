345b8ee67d3967433d4b3d44a1533fa40912ecfd
Tyler Barnes <tylerdbarnes@gmail.com>
2021-04-22 10:00:29 -0700

    fix postcss issue by copying temporarily copying Gutenbergcss into this project

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,8 +4,12 @@ import Image from "gatsby-image"
 import parse from "html-react-parser"
 
 // We're using Gutenberg so we need the block styles
-import "@wordpress/block-library/build-style/style.css"
-import "@wordpress/block-library/build-style/theme.css"
+// these are copied into this project due to a conflict in the postCSS
+// version used by the Gatsby and @wordpress packages that causes build
+// failures.
+// @todo update this once @wordpress upgrades their postcss version
+import "../css/@wordpress/block-library/build-style/style.css"
+import "../css/@wordpress/block-library/build-style/theme.css"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
